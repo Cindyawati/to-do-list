29e257df0b83bb0a420c52a3da08b4646a2ba698
Cindy Aristawati <61828037+Cindyawati@users.noreply.github.com>
2025-08-02 15:24:40 +0800

    1st  Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Todo from './components/Todo';
+
+// https://www.youtube.com/watch?v=WE8aYoGK0Ec&t=1104s
+
+const App = () => {
+  return (
+    <div className="bg-stone-900 grid py-4 min-h-screen">
+      <Todo />
+    </div>
+  );
+};
+
+export default App;
